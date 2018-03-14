4c11cd89b73a1e3e4296918f3d1dced3c6540ca9
Robert Firstman <robertfirstman@lawn-128-61-36-36.lawn.gatech.edu>
2018-03-14 00:35:51 -0400

    bug fix

--- bot/bot.js
+++ bot/bot.js
@@ -27,16 +27,16 @@ module.exports = {
 function processMessage(message) {
     if (message.includes("@whosecar")) {
         var response = generateGreeting() + "\nWhose Car:\n";
-        if (message.includes(/walz/i)) {
+        if (message.match(/walz/i)) {
             return "BEEP BOOP FUCK WALZ";
         }
-        if (message.includes(/josh/i)) {
+        if (message.match(/josh/i)) {
             return "Josh is a turd wrangler";
         }
-        if (message.includes(/everett/i)) {
+        if (message.match(/everett/i)) {
             return "TRIGGERED";
         }
-        if (message.includes("/clint/i")) {
+        if (message.match("/clint/i")) {
             return "Oh no!! Clint's blocking the back lot again? Whatever can we do?";
         }
         if (message.length - 9 <= 1) {
