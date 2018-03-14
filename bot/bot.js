0c670613916168a69ebdfb81afd981f6db0fc60a
Robert Firstman <robertfirstman@lawn-128-61-36-36.lawn.gatech.edu>
2018-03-14 00:05:08 -0400

    testing

--- bot/bot.js
+++ bot/bot.js
@@ -10,7 +10,8 @@ module.exports = {
         var options = {
             method: "POST",
             url: "https://api.groupme.com/v3/bots/post",
-            data: { text: response, bot_id: "4985f806e5462413b4cd75b06e" }
+            //data: { text: response, bot_id: "4985f806e5462413b4cd75b06e" }
+            data: { text: response, bot_id: "469361e7f4e353c11f4e4ce7c9" }
         };
 
         curl.request(options, function(error, response) {
