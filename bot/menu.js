df935868b3a5ffa25c8f7e1c19e460ff9d2373d5
Robert Firstman <robfirstman@gmail.com>
2018-03-15 22:00:14 -0400

    Can now get today's menu

--- bot/menu.js
+++ bot/menu.js
@@ -1,14 +1,22 @@
-module.exports = () => {
-	try {
-		const menu = require("../data/menu.json");
-		let response = "\n";
-		for (var key in menu) {
-			let { lunch, dinner } = menu[key]
-			response += key + ":\n" + lunch + "\n" + dinner + "\n";
+const menu = require("../data/menu.json");
+const days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+
+module.exports = (args) => {
+	if (args.includes("today")) {
+		let n = new Date().getDay();
+		if (n == 0 || n == 6) {
+			return "No food on the weekend!";
 		}
-		return response;
-	} catch (err) {
-		console.log(err);
-		return "Couldn't get the menu :(";
+		const today = days[n];
+		let { lunch, dinner } = menu[today];
+
+		return "\nToday's Menu:\n" + lunch + "\n" + dinner + "\n"; 
 	}
+
+	let response = "\n";
+	for (var key in menu) {
+		let { lunch, dinner } = menu[key]
+		response += key + ":\n" + lunch + "\n" + dinner + "\n";
+	}
+	return response;
 }
