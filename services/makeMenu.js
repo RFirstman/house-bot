ba99a334a296dff502fea0466958bb65722f2ce9
Robert Firstman <robfirstman@gmail.com>
2018-03-15 20:50:32 -0400

    can now respond with the weekly menu

---
+++ services/makeMenu.js
@@ -0,0 +1,61 @@
+var mammoth = require("mammoth");
+var path = require("path");
+var fs = require("fs");
+
+const daysRegex = /Monday|Tuesday|Wednesday|Thursday|Friday/i;
+
+const main = async () => {
+	let buffer = fs.readFileSync(
+		path.resolve(__dirname, "..", "input.docx"),
+		"binary"
+	);
+
+	const text = (await mammoth.extractRawText({ buffer })).value;
+	const lines = text.split("\n");
+
+	console.log(lines);
+	let menu = {};
+	let i = 0;
+	while (i < lines.length) {
+		let line = lines[i];
+		if (daysRegex.test(line)) {
+			let day = line.match(daysRegex);
+			menu[day] = {};
+
+			do {
+				i++;
+			} while (lines[i].length == 0);
+
+			let lunchText = "",
+				dinnerText = "",
+				count = 0;
+			while (i < lines.length && count < 2) {
+				if (lines[i].length < 1) {
+					count++;
+				} else {
+					lunchText += lines[i] + "\n";
+					count = 0;
+				}
+				i++;
+			}
+			menu[day].lunch = lunchText;
+
+			count = 0;
+			while (i < lines.length && count < 2) {
+				if (lines[i].length < 1) {
+					count++;
+				} else {
+					dinnerText += lines[i] + "\n";
+					count = 0;
+				}
+				i++;
+			}
+			menu[day].dinner = dinnerText;
+		}
+		i++;
+	}
+
+	fs.writeFileSync(path.resolve(__dirname, "..", "data", "menu.json"), JSON.stringify(menu,null,4));
+};
+
+main().catch(error => console.error(error));
