4b1070d0541a0b5c8783086d4ed4dc7784176e93
Robert Firstman <robfirstman@gmail.com>
2018-03-15 22:45:37 -0400

    added help command

---
+++ bot/help.js
@@ -0,0 +1,10 @@
+module.exports = () => {
+	return `Available commands:
+		menu
+		 	- get this week's menu 
+		 	- add 'today' for today's menu
+		weather
+			 - get the current weather
+		whosecar <brand/make>
+			- returns who the <brand/make> belongs to`;
+}
