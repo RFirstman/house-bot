baa0dfd8207b85f38af1f451e0e608b07b3da05d
Robert Firstman <robfirstman@gmail.com>
2018-04-27 00:06:46 -0400

    Users can now get menu for the next day

--- bot/help.js
+++ bot/help.js
@@ -1,10 +1,12 @@
 module.exports = () => {
-	return `Available commands:
-		menu
-		 	- get this week's menu 
-		 	- add 'today' for today's menu
-		weather
-			 - get the current weather
-		whosecar <brand/make>
-			- returns who the <brand/make> belongs to`;
+	return 
+	`Available commands:
+	  menu
+	    - get this week's menu 
+	    - add 'today' for today's menu
+	    - or 'tomorrow' for tomorrow's
+	  weather
+	    - get the current weather
+	  whosecar <brand/make>
+		- returns who the <brand/make> belongs to`;
 }
--- bot/menu.js
+++ bot/menu.js
@@ -15,12 +15,21 @@ module.exports = async args => {
 	if (args.includes("today")) {
 		let n = new Date().getDay();
 		if (n == 0 || n == 6) {
-			return "No food on the weekend!";
+			return "\nNo food on the weekend!";
 		}
 		const today = days[n];
 		let { lunch, dinner } = menu[today];
 
 		return "\nToday's Menu:\n" + lunch + "\n" + dinner + "\n";
+	} else if (args.includes("tomorrow")) {
+		let n = (new Date().getDay() + 1) % 7;
+		if (n == 0 || n == 6) {
+			return "\nNo food on the weekend!";
+		}
+		const tomorrow = days[n];
+		let { lunch, dinner } = menu[tomorrow];
+
+		return "\n" + tomorrow + "'s Menu:\n" + lunch + "\n" + dinner + "\n";
 	}
 
 	let response = "\n";
